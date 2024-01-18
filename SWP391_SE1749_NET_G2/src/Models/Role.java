ae7ce414f9ae4a69515de698ffde4d209d1e714d
hungnv02k3 <146454231+hungnv02k3@users.noreply.github.com>
2024-01-18 16:51:24 +0700

    Create-Model
    Role model

---
+++ SWP391_SE1749_NET_G2/src/Models/Role.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author User
+ */
+public class Role {
+    private int RoleID;
+    private String RoleName;
+
+    public Role(int RoleID, String RoleName) {
+        this.RoleID = RoleID;
+        this.RoleName = RoleName;
+    }
+
+    public Role() {
+    }
+
+    public int getRoleID() {
+        return RoleID;
+    }
+
+    public void setRoleID(int RoleID) {
+        this.RoleID = RoleID;
+    }
+
+    public String getRoleName() {
+        return RoleName;
+    }
+
+    public void setRoleName(String RoleName) {
+        this.RoleName = RoleName;
+    }
+    
+}
